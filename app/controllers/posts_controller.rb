7a532827b76c2390025ba3d8cafb0d8a9c1ccbb9
dlaf88 <dlaf88@gmail.com>
2016-10-13 01:41:15 +0000

    added new Posts pages

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,25 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show]
+  def show    
+  end
   def index
+  end
+  def new
+    @post = Post.new
+  end 
+  def create
+    @post = Post.new(post_params)
+    if @post.save 
+      redirect_to post_path(@post), notice: 'Post has been sucessfully created.'
+    else 
+      render :new
+    end
+  end
+  private
+  def set_post
+    @post = Post.find(params[:id])
+  end
+  def post_params
+    params.require(:post).permit(:date,:rationale)
   end
 end
