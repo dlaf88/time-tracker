f49c6a427296e208da18fe2c0530fad1424e0622
dlaf88 <dlaf88@gmail.com>
2016-10-17 19:50:34 +0000

    implemented status functionality

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -38,6 +38,6 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])    
   end
   def post_params
-    params.require(:post).permit(:date,:rationale)
+    params.require(:post).permit(:date,:rationale,:status)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,10 @@
 
 puts "1 User created"
 
+AdminUser.create(email: "admin@admin.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "User", last_name: "Admin")
+
+puts "1 Admin User created"
+
 100.times do |post|
 	Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id)
 end
