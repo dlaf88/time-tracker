277a4a3ffa6c2c2d31318ad600d37ad169dd9bbd
dlaf88 <dlaf88@gmail.com>
2016-10-23 13:32:38 +0000

    finished section 15

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,10 @@ class User < ActiveRecord::Base
   has_many :posts
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  validates_presence_of :first_name, :last_name, :phone
-  
+  validates_presence_of :first_name, :last_name, :phone  
+  REGEX = /\A[0-9]*\Z/
+  validates_format_of :phone, with: REGEX
+  validates :phone, length:{is: 10}
   def full_name
     "#{last_name.upcase}" + ', ' + "#{first_name.upcase}"
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,7 +8,9 @@ RSpec.describe User, type: :model do
     it 'can be created' do
       expect(@user).to be_valid
     end
-    it 'cannot be created without names' do
+  end
+  describe "validation" do 
+     it 'cannot be created without names' do
       @user.first_name = nil
       @user.last_name = nil
       expect(@user).to_not be_valid
@@ -17,8 +19,16 @@ RSpec.describe User, type: :model do
       @user.phone = nil 
       expect(@user).to_not be_valid
     end 
-
-  end
+    
+    it 'phone attr must be composed of integers ' do 
+      @user.phone = 'mygreatstr'
+      expect(@user).to_not be_valid
+    end 
+     it 'phone attr must be 10 characters long ' do 
+      @user.phone = '12345678910111'
+      expect(@user).to_not be_valid
+    end 
+  end 
   describe "custom name methods" do
   it 'has a full name method that combines first and last name' do
       expect(@user.full_name).to eq("SNOW, JON")
