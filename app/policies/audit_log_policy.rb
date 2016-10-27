b85b2a015e3091f33ef175ec9d8b31ac7435deaa
dlaf88 <dlaf88@gmail.com>
2016-10-27 13:27:50 +0000

    finished lecture 89

--- app/policies/audit_log_policy.rb
+++ app/policies/audit_log_policy.rb
@@ -3,7 +3,9 @@ class AuditLogPolicy < ApplicationPolicy
   def index?     
     return true if admin?
  end
-
+ def confirm?
+   record.user_id == user.id
+ end 
 private
   def admin?
     admin_types.include?(user.type)
